3f33c1bb88644db1550a998fbd3bf438d4b4ca82
janiapurva <jani.apurva@yahoo.com>
2021-01-06 17:17:03 -0500

    adding findKeyValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual,expected) {
+  console.log("actual" , actual , "expected" , expected);
+  if (actual === expected) {
+    console.log(`Assertion passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion failed  ${actual} !== ${expected}`);
+  }
+   
+};
+
+function findKeyByValue(list,value){
+  for(let elemenent in list){
+
+  if(list[elemenent] === value){
+    return elemenent
+  }
+  }
+ return undefined;
+}
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+
+
