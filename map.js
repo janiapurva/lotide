948bcf7f771abe42a27dba5a04aab90391436e7a
janiapurva <jani.apurva@yahoo.com>
2021-01-07 15:40:34 -0500

    adding map.js file

---
+++ map.js
@@ -0,0 +1,49 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+
+ const map = function (array,callabck) {
+   const results = [];
+   for (let item of array) {
+     results.push(callabck(item))
+     
+   }
+   return results;
+  
+}
+
+const result1 = map(words,word => word [0]);
+console.log(result1)
+
+
+const assertArraysEqual = function(actual,expected) {
+  if(actual.length !== expected.length){
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`Assertion failed  ${actual} !== ${expected}`);
+    }
+  }
+  console.log(`Assertion passed : ${actual} === ${expected}`);
+};
+
+
+const eqArrays = function(actual,expected) {
+  if(actual.length !== expected.length){
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+      
+    }
+  }
+   return true;
+};
+
+const input = [1,2,3,4,5]
+const output = map(input, a => a*2)
+console.log(eqArrays(output,[2,4,6,8,10]))
+console.log(eqArrays(output,[2,4,8,8,10]))
+console.log(eqArrays(output,[2,3,8,8,10]))
