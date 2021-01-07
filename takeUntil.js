e9669b2daf5e8f834402960dc206ae1b1d64a04a
janiapurva <jani.apurva@yahoo.com>
2021-01-07 18:07:06 -0500

    adding take until.js

---
+++ takeUntil.js
@@ -0,0 +1,18 @@
+const takeUntil = function(array,callback) {
+  let result = []
+  for (const item of array) {
+    if(!callback(item)){
+    result.push(item)
+    
+} else {
+  return result;
+  
+}
+  }
+  
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
