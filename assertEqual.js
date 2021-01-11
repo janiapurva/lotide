e446343266b4c7294e7506e5b80accb7666b1db4
janiapurva <jani.apurva@yahoo.com>
2021-01-11 09:27:05 -0500

    adding refactor

--- assertEqual.js
+++ assertEqual.js
@@ -1,20 +1,13 @@
-// const assertEqual = function(actual, expected) {
-//   console.assert(actual === expected, 'Assertion Passed:', [actual] === [expected])
-//   console.assert(actual !== expected, 'Asssertiona Failed:', [actual] !== [expected])
-
-// };
-
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
 
 const assertEqual = function(actual,expected) {
   console.log("actual" , actual , "expected" , expected);
   if (actual === expected) {
-    console.log(`Assertion passed : ${actual} === ${expected}`);
+    console.log(`🟢 🟢 🟢 Assertion passed : ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion failed  ${actual} !== ${expected}`);
+    console.log(`🔴 🔴 🔴 Assertion failed  ${actual} !== ${expected}`);
   }
    
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,13 +1,4 @@
-const assertEqual = function(actual,expected) {
-  console.log("actual" , actual , "expected" , expected);
-  if (actual === expected) {
-    console.log(`Assertion passed : ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion failed  ${actual} !== ${expected}`);
-  }
-   
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
+const assertEqual = require('./assertEqual');
 
 
 function head(array) {
