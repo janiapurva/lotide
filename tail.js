1e82875913b1f2147ec9f93bea6f1e30c4729369
janiapurva <jani.apurva@yahoo.com>
2021-01-11 09:40:07 -0500

    adding refactor

--- tail.js
+++ tail.js
@@ -1,12 +1,5 @@
-const assertEqual = function(actual,expected) {
-  console.log("actual" , actual , "expected" , expected);
-  if (actual === expected) {
-    console.log(`Assertion passed : ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion failed  ${actual} !== ${expected}`);
-  }
-   
-};
+const assertEqual = require('./assertEqual');
+
 
 
 function tail(array) {
