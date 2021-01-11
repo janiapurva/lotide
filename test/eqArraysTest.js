0fb25c4f25193852db609bcc87050f0464104222
janiapurva <jani.apurva@yahoo.com>
2021-01-11 14:08:21 -0500

    adding refactor test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,1 +1,5 @@
 const assertEqual = require('../eqArrays');
+const eqArrays = require('../eqArrays')
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),[1,2,3] )
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),[3,2,1])
